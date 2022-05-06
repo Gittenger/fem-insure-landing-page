91969a946e2b45c4d64db7b8ab34c2d725be9151
John Pittenger <36045366+Gittenger@users.noreply.github.com>
2022-05-06 13:42:38 -0400

    Updates

---
+++ precache-manifest.44362236caab8e7e94b12c4dd7c94d16.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af944d6c34be1fed47ba10395d37de54",
+    "url": "/fem-insure-landing-page/index.html"
+  },
+  {
+    "revision": "33f2629508f4a068a4fc",
+    "url": "/fem-insure-landing-page/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "edc7e9b9354e04831e6f",
+    "url": "/fem-insure-landing-page/static/js/2.9c077bb8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/fem-insure-landing-page/static/js/2.9c077bb8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "33f2629508f4a068a4fc",
+    "url": "/fem-insure-landing-page/static/js/main.dbd7e44f.chunk.js"
+  },
+  {
+    "revision": "17fe3770cfb94cf77931",
+    "url": "/fem-insure-landing-page/static/js/runtime-main.99770396.js"
+  },
+  {
+    "revision": "1586c300d23ec651816b4fbe776ce89a",
+    "url": "/fem-insure-landing-page/static/media/bg-pattern-footer-desktop.1586c300.svg"
+  },
+  {
+    "revision": "4abb74643a40d67264d0e192655faa22",
+    "url": "/fem-insure-landing-page/static/media/bg-pattern-how-we-work-desktop.4abb7464.svg"
+  },
+  {
+    "revision": "600d644489d031fb0e8be595b58bfa37",
+    "url": "/fem-insure-landing-page/static/media/bg-pattern-intro-left-desktop.600d6444.svg"
+  },
+  {
+    "revision": "fc61cf1f05af8c8a0a1589173cd5a6a6",
+    "url": "/fem-insure-landing-page/static/media/bg-pattern-intro-right-desktop.fc61cf1f.svg"
+  },
+  {
+    "revision": "b9dc0b5c6bd0cf4ddcd5ce9f3ad16897",
+    "url": "/fem-insure-landing-page/static/media/icon-affordable-prices.b9dc0b5c.svg"
+  },
+  {
+    "revision": "6fd0e32479e7d8af8e471d2a34244e93",
+    "url": "/fem-insure-landing-page/static/media/icon-facebook.6fd0e324.svg"
+  },
+  {
+    "revision": "48a204c8175de66e9c8f1b9207cc77eb",
+    "url": "/fem-insure-landing-page/static/media/icon-instagram.48a204c8.svg"
+  },
+  {
+    "revision": "b264095b9d9862f3cc93d449508b65fd",
+    "url": "/fem-insure-landing-page/static/media/icon-people-first.b264095b.svg"
+  },
+  {
+    "revision": "275a65a2fef7bff622ba4fb6e7aa57eb",
+    "url": "/fem-insure-landing-page/static/media/icon-pinterest.275a65a2.svg"
+  },
+  {
+    "revision": "3cc5dc39b6297427126b6a7389e9b76d",
+    "url": "/fem-insure-landing-page/static/media/icon-snappy-process.3cc5dc39.svg"
+  },
+  {
+    "revision": "6eef62ff8487d0c9808c68e74d68d9c8",
+    "url": "/fem-insure-landing-page/static/media/icon-twitter.6eef62ff.svg"
+  },
+  {
+    "revision": "8ac69ec7c30fa6465fa05426a4b235f4",
+    "url": "/fem-insure-landing-page/static/media/image-intro-desktop.8ac69ec7.jpg"
+  },
+  {
+    "revision": "09a91c1a381a6fc1ecb8512490b74a12",
+    "url": "/fem-insure-landing-page/static/media/image-intro-mobile.09a91c1a.jpg"
+  },
+  {
+    "revision": "1bc452ac6f5856ef32a25e4519a4db91",
+    "url": "/fem-insure-landing-page/static/media/logo.1bc452ac.svg"
+  }
+]);
